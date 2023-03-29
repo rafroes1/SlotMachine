968855ab7b35b87d06a76779da3e1f3c5a3162ec
rafroes1 <rafafroes@outlook.com>
2023-03-29 08:42:21 -0300

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Rafael Carvalho on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
